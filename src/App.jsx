877586b78095fb660236f27aefd54d574c711a42
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-09-09 10:29:39 +0530

    Initial dashboard setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/globals.css";
+import Dashboard from "./pages/dashboard/dashboard";
 
 function App() {
-
   return (
     <>
-      <div></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
